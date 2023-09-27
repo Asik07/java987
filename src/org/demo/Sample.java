336d6c012e353984ae0aa6ace3a28e71fbdbe8f4
harishgreens <mohamedharish@gmail.com>
2023-09-27 09:40:13 +0530

    done

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -1,6 +1,10 @@
 package org.demo;
 
 public class Sample {
+	
+	private void method2() {
+System.out.println("abdul");
+	}
 public static void main(String[] args) {
 	
 	System.out.println("done");
