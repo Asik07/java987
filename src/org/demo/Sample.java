3c7f720011273238c66d7e97f449627f17769faf
harishgreens <mohamedharish@gmail.com>
2023-09-27 09:33:21 +0530

    add

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -1,6 +1,12 @@
 package org.demo;
 
 public class Sample {
+	
+	private void method1() {
+System.out.println("asik");
+	}
+	
+	
 public static void main(String[] args) {
 	
 	System.out.println("done");
