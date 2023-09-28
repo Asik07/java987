3f1b66063f720cb9c792fa320358dda7b351a273
harishgreens <mohamedharish@gmail.com>
2023-09-28 09:23:56 +0530

    add

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -1,6 +1,12 @@
 package org.demo;
 
 public class Sample {
+	
+	private void method4() {
+System.out.println("praveen");
+	}
+	
+	
 public static void main(String[] args) {
 	
 	System.out.println("done");
